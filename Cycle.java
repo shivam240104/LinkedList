82a77c211e0178f7cc4c98cb3f366636a14bad0d
shivam240104 <mauryatanmay845@gmail.com>
2024-09-18 20:43:58 +0530

    To remove cycle in LinkedList

--- Cycle.java
+++ Cycle.java
@@ -24,11 +24,40 @@ public class Cycle {
         return false;
     }
 
+    public static void removeCycle(){
+        Node slow= head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast !=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast==slow){
+                cycle =true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return ;
+        }
+        slow =head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow =slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+    }
+
     public static void main(String[] args) {
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next= new Node(3);
-        head.next.next.next= head;
+        head.next.next.next= temp;
         System.out.println(Iscycle());
+        removeCycle();
+        System.out.println(Iscycle());
+
     }
 }
