1fce247e8d64460df0c94c0ff765971797f10836
shivam240104 <mauryatanmay845@gmail.com>
2024-09-19 18:57:27 +0530

    to use java collection framework

---
+++ JCF.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+public class JCF {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+    }
+}
